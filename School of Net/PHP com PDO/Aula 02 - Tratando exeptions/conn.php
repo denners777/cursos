3a4c0c2cce600bbf4b754b0018102b1d88acdbba
Denner Fernandes <denners777@hotmail.com>
2016-12-17 03:11:26 -0200

    PHP com PDO Dia 1 (correção)
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

---
+++ School of Net/PHP com PDO/Aula 02 - Tratando exeptions/conn.php
@@ -0,0 +1,11 @@
+<?php
+
+try{
+
+	$conn = new \PDO('mysql:host=localhost;dbname=test_oo', 'root', 'root');
+
+}catch(\PDOException $e){
+  	
+  	echo 'Erro! Message: ' . $e->getMessage() . ' Code: ' . $e->getCode();
+  	
+}
