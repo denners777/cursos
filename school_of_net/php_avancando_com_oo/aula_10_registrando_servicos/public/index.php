e1e86cb752ca42195b15b53bcc7d1a14f6c5a153
Denner Fernandes <denners777@hotmail.com>
2017-03-12 14:11:33 -0300

    php_avancando_com_oo\aula_10_registrando_servicos
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

---
+++ school_of_net/php_avancando_com_oo/aula_10_registrando_servicos/public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Pimple\Container;
+
+$container = new Container();
+
+$container['conn'] = function() {
+    return new \Source\Conn('mysql:host=localhost;dname=test_oo', 'root', '');
+};
+
+$container['product'] = function($c) {
+    return new \Source\Product($c['conn']);
+};
+
+$list = $container['product']->lister();
+
+echo '<xmp>', print_r($list);
