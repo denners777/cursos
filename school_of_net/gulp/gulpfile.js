a9c1d60482fcd5e8eae9a36febc118b2b645f29b
Denner Fernandes <denners777@hotmail.com>
2017-03-15 20:52:16 -0300

    gulp.concat
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- school_of_net/gulp/gulpfile.js
+++ school_of_net/gulp/gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        concat = require('gulp-concat');
 
 gulp.task('sass', function () {
   return gulp.src('assets/src/sass/**/*.scss')
+          .pipe(concat('style.min.css'))
           .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
           .pipe(gulp.dest('assets/css'));
 });
