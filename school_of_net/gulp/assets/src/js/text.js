caac9f03fd86e5740a11ed07bea3a6462460b24e
Denner Fernandes <denners777@hotmail.com>
2017-03-15 22:01:17 -0300

    gulp.minificar_imagens
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- school_of_net/gulp/assets/src/js/text.js
+++ school_of_net/gulp/assets/src/js/text.js
@@ -1,6 +1,6 @@
 var app = (function(document){
   document.getElementById("logo").innerHTML = "Gulp";
   document.getElementById("logoDesc").innerHTML = "Automate and enhance your workflow";
-  document.getElementById("video").innerHTML = "gulp-uglify";
+  document.getElementById("video").innerHTML = "gulp-imagemin";
 })(document);
 
--- school_of_net/gulp/gulpfile.js
+++ school_of_net/gulp/gulpfile.js
@@ -3,7 +3,15 @@
 var gulp = require('gulp'),
         sass = require('gulp-sass'),
         concat = require('gulp-concat'),
-        uglify = require('gulp-uglify');
+        uglify = require('gulp-uglify'),
+        imagemin = require('gulp-imagemin'),
+        pngquant = require('imagemin-pngquant'),
+        imageminGifsicle = require('imagemin-gifsicle'),
+        imageminJpegtran = require('imagemin-jpegtran'),
+        imageminOptipng = require('imagemin-optipng'),
+        imageminSvgo = require('imagemin-svgo');
+
+gulp.task('default', ['sass', 'js', 'image']);
 
 gulp.task('sass', function () {
   return gulp.src('assets/src/sass/**/*.scss')
@@ -19,4 +27,22 @@ gulp.task('js', function () {
           .pipe(gulp.dest('assets/js'));
 });
 
-gulp.task('default', ['sass', 'js']);
+gulp.task('image', function () {
+  return gulp.src('assets/src/img/*')
+          .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [
+              pngquant(),
+              imageminGifsicle(),
+              imageminJpegtran(),
+              imageminOptipng(),
+              imageminSvgo({
+                plugins: [
+                  {removeViewBox: false}
+                ]
+              }),
+            ]
+          }))
+          .pipe(gulp.dest('assets/img'));
+});
