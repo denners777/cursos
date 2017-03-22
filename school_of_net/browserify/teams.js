86cba04c31f229cd13c70c3359bbeae286c53897
Denner Fernandes <denners777@hotmail.com>
2017-03-21 21:53:39 -0300

    browserify.commonjs
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

---
+++ school_of_net/browserify/teams.js
@@ -0,0 +1,16 @@
+var teams = [
+  'Brazil',
+  'Germany',
+  'Italy',
+  'France',
+];
+
+/*var result = teams.filter(function (value) {
+ return value == 'Italy';
+ });*/
+
+//module.exports = result;
+
+module.exports = function () {
+  console.log(teams);
+};
