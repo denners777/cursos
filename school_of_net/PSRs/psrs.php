664869b55aac3d58ac49da49bb7ceb5887d1f921
Denner Fernandes <denners777@hotmail.com>
2017-04-03 22:30:58 -0300

    PSRs.php_fig_e_as_psrs

---
+++ school_of_net/PSRs/psrs.php
@@ -0,0 +1,15 @@
+<?php
+/*
+http://www.php-fig.org/
+
+AUTOLOADING
+Os autoloaders removem a complexidade de incluir arquivos ao mapear namespaces para caminhos do sistema de arquivos.
+PSR-4: Autoloading com melhor aspecto 
+
+INTERFACES
+As interfaces simplificam o compartilhamento de código entre projetos seguindo os contratos esperados.
+PSR-3: Interface de Logger PSR-7: Interfaces de Mensagem HTTP
+
+CODING STYLES
+A formatação padronizada reduz o atrito cognitivo ao ler o código de outros autores.
+PSR-1: Codificação básica padrão PSR-2: Guia de estilo de codificação
