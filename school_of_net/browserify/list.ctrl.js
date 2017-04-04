3c1ef982b540cc50e2725b61703748fd86ee2721
Denner Fernandes <denners777@hotmail.com>
2017-04-03 21:38:30 -0300

    browserify.produzindo_uma_agenda_delete
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- school_of_net/browserify/list.ctrl.js
+++ school_of_net/browserify/list.ctrl.js
@@ -1,14 +1,22 @@
 module.exports = function ($scope, Contacts) {
-  var obj = [{
-      name: 'Denner',
-      telefone: '55 5565-6566',
-    },{
-      name: 'Zé',
-      telefone: '55 6569-8989',
-    },{
-      name: 'João',
-      telefone: '21 5689-9632',
-    }]
+//  var obj = [{
+//      name: 'Denner',
+//      telefone: '55 5565-6566',
+//    },{
+//      name: 'Zé',
+//      telefone: '55 6569-8989',
+//    },{
+//      name: 'João',
+//      telefone: '21 5689-9632',
+//    }]
+//
+//  $scope.data = Contacts.setData(obj);
 
-  $scope.data = Contacts.setData(obj);
+  $scope.data = Contacts.getData();
+
+  $scope.remove = function (item) {
+    var index = Contacts.getData().indexOf(item);
+    Contacts.getData().splice(index, 1);
+  }
+
 };
