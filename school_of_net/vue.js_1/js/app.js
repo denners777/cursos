a5fe0b5d9effd318e830d14ea2589ec69f941264
Denner Fernandes <denners777@hotmail.com>
2017-05-07 14:12:21 -0300

    vue.js_1.eventos_e_metodos

--- school_of_net/vue.js_1/js/app.js
+++ school_of_net/vue.js_1/js/app.js
@@ -8,6 +8,21 @@ var hello = new Vue({
             {name: 'Gustavo'},
             {name: 'Ana'},
             {name: 'Gabriela'},
-        ]
+        ],
+        newElement: '',
+        elements: [],
     },
+    methods: {
+        addElement: function () {
+            var title = this.newElement.trim();
+            if (title) {
+                this.elements.push({title: title});
+                this.newElement = '';
+            }
+        },
+        removeElement: function (e, index) {
+            e.preventDefault();
+            this.elements.splice(index, 1);
+        }
+    }
 });
