4416a253ff9d94e374c218d882ba8f34941ea777
Denner Fernandes <denners777@hotmail.com>
2017-05-08 22:41:50 -0300

    vue.js_1.paginacaão_parte_2
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- school_of_net/vue.js_1/js/app.js
+++ school_of_net/vue.js_1/js/app.js
@@ -5,6 +5,14 @@ var app = new Vue({
         MySearch: '',
         orderCol: 'id',
         orderInverse: 1,
+        pagination: {
+            maxPage: 4,
+            current: 1,
+            totalItems: 0,
+            totalPages: 5,
+            listNumbers: [],
+            listPagination: [],
+        },
     },
     methods: {
         filterOrderBy: function (e, col) {
@@ -15,12 +23,42 @@ var app = new Vue({
         },
         previous: function (e) {
             e.preventDefault();
+            
+            if(this.pagination.current === 1){
+                return false;
+            }
+            this.pagination.current = this.pagination.current - 1;
+            
+            console.log('maxPage: ' + this.pagination.maxPage);
+            console.log('current: ' + this.pagination.current);
+            console.log('totalItems: ' + this.pagination.totalItems);
+            console.log('totalPages: ' + this.pagination.totalPages);
+            console.log('listNumbers: ' + this.pagination.listNumbers);
+            console.log('listPagination: ' + this.pagination.listPagination);
         },
         pagePagination: function (e, id) {
             e.preventDefault();
+            
+            if(this.pagination.current === this.pagination.totalPages){
+                return false;
+            }
+            this.pagination.current = this.pagination.current + 1;
+            
+            console.log('maxPage: ' + this.pagination.maxPage);
+            console.log('current: ' + this.pagination.current);
+            console.log('totalItems: ' + this.pagination.totalItems);
+            console.log('totalPages: ' + this.pagination.totalPages);
+            console.log('listNumbers: ' + this.pagination.listNumbers);
+            console.log('listPagination: ' + this.pagination.listPagination);
         },
         next: function (e) {
             e.preventDefault();
+            console.log('maxPage: ' + this.pagination.maxPage);
+            console.log('current: ' + this.pagination.current);
+            console.log('totalItems: ' + this.pagination.totalItems);
+            console.log('totalPages: ' + this.pagination.totalPages);
+            console.log('listNumbers: ' + this.pagination.listNumbers);
+            console.log('listPagination: ' + this.pagination.listPagination);
         },
     },
     ready: function () {
