6774fc127d2a4a0332885953a341e32383edefc6
Denner Fernandes <denners777@hotmail.com>
2017-05-09 21:30:05 -0300

    vue.js_1.paginacao_parte_5
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- school_of_net/vue.js_1/js/app.js
+++ school_of_net/vue.js_1/js/app.js
@@ -18,7 +18,6 @@ var app = new Vue({
             e.preventDefault();
             this.orderCol = col;
             this.orderInverse = this.orderInverse * (-1);
-            //console.log(this.orderInverse);
         },
         previous: function (e) {
             e.preventDefault();
@@ -29,9 +28,10 @@ var app = new Vue({
             this.pagination.current = this.pagination.current - 1;
             this.books = this.pagination.listPagination[this.pagination.current - 1];
         },
-        pagePagination: function (e, id) {
+        pagePagination: function (e, current) {
             e.preventDefault();
-
+            this.pagination.current = current + 1;
+            this.books = this.pagination.listPagination[current];
         },
         next: function (e) {
             e.preventDefault();
@@ -46,7 +46,7 @@ var app = new Vue({
         var self = this;
         self.$http.get('dataServer.json').then(function (response) {
             self.pagination.totalItems = response.data.length;
-            self.pagination.totalPages = Math.ceil(response.data.lenght / self.pagination.maxPage);
+            self.pagination.totalPages = Math.ceil(response.data.length / self.pagination.maxPage);
 
             var aux = [];
             for (var k in response.data) {
