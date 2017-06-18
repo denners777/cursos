fea53399411c69867021ef344054160dfa2d4f9c
Denner Fernandes <denners777@hotmail.com>
2017-06-17 23:33:06 -0300

    doctrine.crud_de_posts
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- school_of_net/doctrine/src/routes.php
+++ school_of_net/doctrine/src/routes.php
@@ -5,7 +5,6 @@ use Slim\Views\PhpRenderer;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequestFactory;
 use Psr\Http\Message\ServerRequestInterface;
-use App\Entity\Category;
 
 $request = ServerRequestFactory::fromGlobals(
                 $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
@@ -21,66 +20,8 @@ $view = new PhpRenderer(__DIR__ . '/../templates/');
 
 $entityManager = getEntityManager();
 
-$map->get('home', '/home', function($request, $response) use($view) {
-    return $view->render($response, 'home.phtml', ['test' => 'Slim PHP view Funcionando']);
-});
-$map->get('categories.list', '/categories', function($request, $response) use($view, $entityManager) {
-    $repository = $entityManager->getRepository(Category::class);
-    $categories = $repository->findAll();
-    return $view->render($response, 'categories/list.phtml', [
-                'categories' => $categories,
-    ]);
-});
-$map->get('categories.create', '/categories/create', function($request, $response) use($view) {
-    return $view->render($response, 'categories/create.phtml');
-});
-$map->post('categories.store', '/categories/store', function(ServerRequestInterface $request, $response) use($view, $entityManager, $generator) {
-
-    $data     = $request->getParsedBody();
-    $category = new Category();
-    $category->setName($data['name']);
-    $entityManager->persist($category);
-    $entityManager->flush();
-    $uri      = $generator->generate('categories.list');
-
-    return new Response\RedirectResponse($uri);
-});
-
-$map->get('categories.edit', '/categories/{id}/edit', function(ServerRequestInterface $request, $response) use($view, $entityManager) {
-    $id         = $request->getAttribute('id');
-    $repository = $entityManager->getRepository(Category::class);
-    $category   = $repository->find($id);
-    return $view->render($response, 'categories/edit.phtml', [
-                'category' => $category,
-    ]);
-});
-
-$map->post('categories.update', '/categories/{id}/update', function(ServerRequestInterface $request, $response) use($view, $entityManager, $generator) {
-    $id         = $request->getAttribute('id');
-    $repository = $entityManager->getRepository(Category::class);
-    $category   = $repository->find($id);
-
-    $data = $request->getParsedBody();
-
-    $category->setName($data['name']);
-    $entityManager->flush();
-    $uri = $generator->generate('categories.list');
-
-    return new Response\RedirectResponse($uri);
-});
-
-$map->get('categories.remove', '/categories/{id}/remove', function(ServerRequestInterface $request, $response) use($view, $entityManager, $generator) {
-    $id         = $request->getAttribute('id');
-    $repository = $entityManager->getRepository(Category::class);
-    $category   = $repository->find($id);
-
-    $entityManager->remove($category);
-    $entityManager->flush();
-    
-    $uri = $generator->generate('categories.list');
-
-    return new Response\RedirectResponse($uri);
-});
+require_once __DIR__ . '/categories.php';
+require_once __DIR__ . '/posts.php';
 
 $matcher = $routerContainer->getMatcher();
 
