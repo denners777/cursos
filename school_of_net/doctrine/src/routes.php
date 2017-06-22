5d7d41d9996c6ffeb81c63f7c04582833bc9e54a
Denner Fernandes <denners777@hotmail.com>
2017-06-22 18:15:54 -0300

    doctrine.filtro_de_posts
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- school_of_net/doctrine/src/routes.php
+++ school_of_net/doctrine/src/routes.php
@@ -1,31 +1,47 @@
 <?php
 
+use App\Entity\Category;
+use App\Entity\Post;
 use Aura\Router\RouterContainer;
-use Slim\Views\PhpRenderer;
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequestFactory;
-use Psr\Http\Message\ServerRequestInterface;
 
-$request = ServerRequestFactory::fromGlobals(
+$request         = ServerRequestFactory::fromGlobals(
                 $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
 );
 
 $routerContainer = new RouterContainer();
+$generator       = $routerContainer->getGenerator();
+$map             = $routerContainer->getMap();
+$view            = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates/');
+$entityManager   = getEntityManager();
 
-$generator = $routerContainer->getGenerator();
+$map->get('home', '/', function (ServerRequestInterface $request, $response) use ($view, $entityManager) {
+    $postsRepository    = $entityManager->getRepository(Post::class);
+    $categoryRepository = $entityManager->getRepository(Category::class);
+    $categories         = $categoryRepository->findAll();
+    $data               = $request->getQueryParams();
 
-$map = $routerContainer->getMap();
-
-$view = new PhpRenderer(__DIR__ . '/../templates/');
-
-$entityManager = getEntityManager();
+    if (isset($data['search']) and $data['search'] != "") {
+        $queryBuilder = $postsRepository->createQueryBuilder('p');
+        $queryBuilder->join('p.categories', 'c')
+                ->where($queryBuilder->expr()->eq('c.id', $data['search']));
+        $posts        = $queryBuilder->getQuery()->getResult();
+    } else {
+        $posts = $postsRepository->findAll();
+    }
+    return $view->render($response, 'home.phtml', [
+                'posts'      => $posts,
+                'categories' => $categories
+    ]);
+});
 
 require_once __DIR__ . '/categories.php';
 require_once __DIR__ . '/posts.php';
 
 $matcher = $routerContainer->getMatcher();
-
-$route = $matcher->match($request);
+$route   = $matcher->match($request);
 
 foreach ($route->attributes as $key => $value){
     $request = $request->withAttribute($key, $value);
@@ -37,8 +53,7 @@ $callable = $route->handler;
 $response = $callable($request, new Response());
 
 if ($response instanceof Response\RedirectResponse) {
-    header("location:{$response->getHeader('location')[0]}");
+    header("location:{$response->getHeader("location")[0]}");
 } elseif ($response instanceof Response) {
     echo $response->getBody();
 }
-
