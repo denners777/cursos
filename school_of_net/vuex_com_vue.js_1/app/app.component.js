96ceb9f13e33cb3e4d362fcb5ceb250e5d181287
Denner Fernandes <denners777@hotmail.com>
2017-07-04 21:57:53 -0300

    vuex_com_vue.js_1/criando_appcomponent
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

---
+++ school_of_net/vuex_com_vue.js_1/app/app.component.js
@@ -0,0 +1,3 @@
+export default{
+    template: require('html!./app.component.html'),
+}
--- school_of_net/vuex_com_vue.js_1/app/main.js
+++ school_of_net/vuex_com_vue.js_1/app/main.js
@@ -1,1 +1,12 @@
-console.log("Hello world!!!")
+import Vue from 'vue';
+import AppComponent from './app.component';
+
+new Vue({
+    el: "#app",
+    data: {
+        message: 'Hello World!!'
+    },
+    components: {
+        'app-component': AppComponent
+    },
+});
