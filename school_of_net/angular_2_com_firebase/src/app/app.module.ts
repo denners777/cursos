0661379ce85ec3f38c9a2b4cfee2e970f1a7b362
Denner Fernandes <denners777@hotmail.com>
2017-07-23 20:55:55 -0300

    angular_2_com_firebase/conhecendo_e_integrando_o_firebase
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

---
+++ school_of_net/angular_2_com_firebase/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyDT4gs0ltWU-_G7w_-nfljco0YIwzY7wuc",
+  authDomain: "cursos-760e1.firebaseapp.com",
+  databaseURL: "https://cursos-760e1.firebaseio.com",
+  projectId: "cursos-760e1",
+  storageBucket: "",
+  messagingSenderId: "258361656437"
+};
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
