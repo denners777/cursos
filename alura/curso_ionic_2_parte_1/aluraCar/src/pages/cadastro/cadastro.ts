8b0c3b7577d7fd2f989dfd6b87c701bfb4a1bab9
Denner Fernandes <denners777@hotmail.com>
2017-07-31 16:07:51 -0300

    curso_ionic_2_parte_1/realizando_agendamentos/melhorando_a_experiencia_do_usuário
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- alura/curso_ionic_2_parte_1/aluraCar/src/pages/cadastro/cadastro.ts
+++ alura/curso_ionic_2_parte_1/aluraCar/src/pages/cadastro/cadastro.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 
 import { Carro } from '../../domain/carro/carro';
 
@@ -18,27 +18,39 @@ export class CadastroPage {
   public endereco: string;
   public email: string;
   public data: string = new Date().toISOString();
+  private _alerta: Alert;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private _http: Http) {
+    private _http: Http,
+    private _alertCtrl: AlertController
+  ) {
 
     this.carro = navParams.get('carro');
     this.precoTotal = navParams.get('precoTotal');
 
+    this._alerta = this._alertCtrl.create({
+      title: 'Aviso',
+      buttons: [{ text: 'Ok' }]
+    });
+
   }
   agenda() {
     let api = `https://aluracar.herokuapp.com/salvarpedido?carro=${this.carro.nome}&nome=${this.nome}&preco=${this.precoTotal}&endereco=${this.endereco}&email=${this.email}&dataAgendamento=${this.data}`;
-    
+
     this._http
       .get(api)
       .toPromise()
-      .then(() => alert('Agendou'))
+      .then(() => {
+        this._alerta.setSubTitle('Agendamento realizado com sucesso!');
+        this._alerta.present();
+      })
       .catch(err => {
         console.log(err);
-        alert('Deu problema!');
+        this._alerta.setSubTitle('Não foi possível realizar o agendamento. Tente mais tarde.');
+        this._alerta.present();
       }
-    );
+      );
   }
 }
