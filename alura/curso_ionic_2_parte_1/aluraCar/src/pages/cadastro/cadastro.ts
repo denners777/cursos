d97b4fe58e4f8e9bff16c75f4b64faaa2d02ef80
Denner Fernandes <denners777@hotmail.com>
2017-07-31 15:38:17 -0300

    curso_ionic_2_parte_1/realizando_agendamentos/a_especificacao_da_api_do_cliente
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- alura/curso_ionic_2_parte_1/aluraCar/src/pages/cadastro/cadastro.ts
+++ alura/curso_ionic_2_parte_1/aluraCar/src/pages/cadastro/cadastro.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { Carro } from '../../domain/carro/carro';
 
+import { Http } from '@angular/http';
+
 @Component({
   selector: 'page-cadastro',
   templateUrl: 'cadastro.html'
@@ -17,13 +19,16 @@ export class CadastroPage {
   public email: string;
   public data: string = new Date().toISOString();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private _http: Http) {
 
     this.carro = navParams.get('carro');
     this.precoTotal = navParams.get('precoTotal');
 
   }
-  agenda(){
-    console.log(this.nome, this.endereco, this.email, this.data);
+  agenda() {
+
   }
 }
