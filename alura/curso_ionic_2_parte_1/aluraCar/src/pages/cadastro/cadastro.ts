fedf3121a914c87cb497ac56603042631eb28f03
Denner Fernandes <denners777@hotmail.com>
2017-07-31 15:28:29 -0300

    curso_ionic_2_parte_1/a_pagina_de_agendamentos/capturando_os_dados_do_formulario
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- alura/curso_ionic_2_parte_1/aluraCar/src/pages/cadastro/cadastro.ts
+++ alura/curso_ionic_2_parte_1/aluraCar/src/pages/cadastro/cadastro.ts
@@ -12,11 +12,18 @@ export class CadastroPage {
   public carro: Carro;
   public precoTotal: number;
 
+  public nome: string;
+  public endereco: string;
+  public email: string;
+  public data: string = new Date().toISOString();
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
     this.carro = navParams.get('carro');
-    console.log(navParams);
     this.precoTotal = navParams.get('precoTotal');
 
+  }
+  agenda(){
+    console.log(this.nome, this.endereco, this.email, this.data);
   }
 }
